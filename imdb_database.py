11fd477dc3a9674712fa9db37197e119234f5a58
JessicaUppal <85931459+JessicaUppal@users.noreply.github.com>
2021-12-11 00:00:17 +0000

    deleted database

--- imdb_database.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-data = pd.read_csv("new_upcoming_dvd_reviews.csv", index_col=0)
-from sqlalchemy import create_engine
-engine = create_engine("postgresql://postgres:April102020!@localhost:5432/movie_review")
-data.to_sql("IMDB_REVIEWS", engine, if_exists="append")
